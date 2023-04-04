b5445c8279aa99281ddd06e4770dead206d1813b
Andrey Kolomoets <kolomoets02@gmail.com>
2023-04-05 01:43:46 +0300

    refactor module13_3

--- Module13/src/main/java/org/example/module13_3/Main.java
+++ Module13/src/main/java/org/example/module13_3/Main.java
@@ -1,41 +1,26 @@
 package org.example.module13_3;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class Main {
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/users/";
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(getOpenTodosForUser(1));
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int userID = 3;
+        getUncompletedTasks(userID);
     }
 
-    public static String getOpenTodosForUser(int userId) throws Exception {
+    static void getUncompletedTasks(int userID) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://jsonplaceholder.typicode.com/users/" + userId + "/todos"))
+                .uri(URI.create("https://jsonplaceholder.typicode.com/users/" + userID + "/todos?completed=false"))
                 .header("Content-Type", "application/json")
                 .GET()
                 .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONArray tasks = new JSONArray(response.body());
-        List<JSONObject> openTasks = new ArrayList<>();
-        for (int i = 0; i < tasks.length(); i++) {
-            JSONObject task = tasks.getJSONObject(i);
-            if (!task.getBoolean("completed")) {
-                openTasks.add(task);
-            }
-        }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return gson.toJson(openTasks);
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
